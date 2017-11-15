fb328a3167a279af7a2e92f93aa5f8094bf633a9
moshenahmed@gmail.com <moshenahmed@gmail.com>
2017-11-15 16:59:05 +0100

    carrot

---
+++ OpenKattisSolutions/OpenKattisSolutions/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OpenKattisSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] input = Console.ReadLine().ToCharArray();
+            if (input.Length == 2) { 
+            int N = int.Parse(Char.ToString(input[0]));
+            int P = int.Parse(Char.ToString(input[1]));
+            
+            if (N >= 1 && P <= 1000)
+            {
+                string names;
+                string[] contestants = new string[N];
+                for (int i = 0; i < N; i++)
+                {
+                    names = Console.ReadLine();
+                    contestants[i] = names;
+                }
+                Console.WriteLine(P);
+            }
+            }
+
+            ////int count = 1;
+            //if (input == 1)
+            //{
+
+            //    Console.WriteLine("Alice");
+            //}
+
+            //else if (input >= 1 && input % 2 == 1)
+            //{
+
+            //    Console.WriteLine("Alice");
+            //}
+
+
+
+            //else
+            //{
+            //    Console.WriteLine("bob");
+            //}
+
+
+        }
+
+
+
+
+    }
+}
+
+
+
+
+
+
